3f578be693f36eaeb0108fd03021e4a1041f0e6f
KIRA-8XY <ariqalwin94004@gmail.com>
2025-06-21 06:47:50 +0700

    Add Indikator model and migration for indikators table

---
+++ app/Models/Indikator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Indikator extends Model
+{
+    //
+    public function up()
+    {
+        Schema::create('indikators', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pegawai_id');
+            $table->string('nama_indikator');
+            $table->double('target');
+            $table->double('realisasi');
+            $table->timestamps();
+
+            $table->foreign('pegawai_id')->references('id')->on('pegawais')->onDelete('cascade');
+        });
+    }
+
+}
