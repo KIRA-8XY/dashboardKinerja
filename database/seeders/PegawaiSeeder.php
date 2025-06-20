7687755a9b69f3786f7b69141da40be95a2ce617
KIRA-8XY <ariqalwin94004@gmail.com>
2025-06-20 15:53:55 +0700

    Add PegawaiSeeder to populate pegawais table with sample data

---
+++ database/seeders/PegawaiSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Pegawai;
+use Illuminate\Database\Seeder;
+
+class PegawaiSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'ANGGA BAGUS SASMITA',
+            'DIMAS WIRA YUDHA',
+            'FAUZIA AMALIA RACHMAWATIE',
+            'HASNI RAHMAWATI',
+            'MEGANSYA PATRIA GANDARA',
+            'R FATKHURRAHMAN ERLANGGA',
+            'RIDHA KARUNIA',
+            'SYAHRIAL FITRIAN AMIR',
+        ];
+
+        foreach ($data as $nama) {
+            Pegawai::create([
+                'nama' => $nama,
+                'target' => rand(50, 100),
+                'realisasi' => rand(30, 100)
+            ]);
+        }
+    }
+}
+
