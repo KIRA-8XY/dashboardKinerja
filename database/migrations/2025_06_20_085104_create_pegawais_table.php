600e55228426a351b8e6daed568c83d40d37004e
KIRA-8XY <ariqalwin94004@gmail.com>
2025-06-20 15:52:52 +0700

    Add Pegawai model and migration for pegawais table

---
+++ database/migrations/2025_06_20_085104_create_pegawais_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('jabatan')->nullable();
+            $table->integer('target')->default(0);
+            $table->integer('realisasi')->default(0);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawais');
+    }
+};
